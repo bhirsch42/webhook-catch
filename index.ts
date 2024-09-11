8192af172b0fff28d2814e5a569242dca88e5e52
Ben Hirsch <bhirsch42@users.noreply.github.com>
2024-09-11 16:43:02 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.post("/test", async (c) => {
+  const body = await c.req.json();
+  console.log("Received payload:", body);
+  return c.text("Webhook received", 200);
+});
+
+export default {
+  port: 4000,
+  fetch: app.fetch,
+};
+
+console.log("Server running at http://localhost:400");
